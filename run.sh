ef80b8ff1866c5b8bf1dfe98e18e285380ab94e8
Maxim Menshikov <maxim@menshikov.org>
2024-02-24 23:31:16 +0000

    Minor improvements

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ TOP_DIR="$(cd "$(dirname "$(which "$0")")" ; pwd -P)"
 . ./lib_header.sh
 
 function get_cmd_line() {
-	echo ./run.sh --port \"${core_port}\" \
-                  --pub-url \"${pub_url}\" \
-                  --pub-fqdn \"${pub_fqdn}\" \
-                  --data-path \"${DISTR_DIR}/data/raw\"
+	echo ./run.sh --port "${core_port}" \
+                  --pub-url "${pub_url}" \
+                  --pub-fqdn "${pub_fqdn}" \
+                  --data-path "../../data/raw"
 }
 
 cmd_line="$(get_cmd_line)"
@@ -15,10 +15,10 @@ cmd_line="$(get_cmd_line)"
 if [ -f "${DISTR_DIR}/data/raw/.firstrun" ] ; then
 	pushd "${DISTR_DIR}/distr/core" > /dev/null
 	FIRST_RUN=y ${cmd_line}
+	status="$?"
 	popd > /dev/null
-	status="$1"
 	if [ "$status" != "0" ] ; then
-		fail "Failed to run Core for the first run"
+		fail "Failed to run Core for the first run: $status"
 		exit $status
 	fi
 
