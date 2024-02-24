e1f8bc765c9d356750cde36470a06ff4f051011a
Maxim Menshikov <maxim@menshikov.org>
2024-02-24 23:49:56 +0000

    New update script

--- update.sh
+++ update.sh
@@ -4,16 +4,36 @@ cd "${TOP_DIR}"
 
 . ./lib_header.sh
 
+read -p "Releases user: " user
+read -p "Releases password: " password
+
+url_release_equestrian="https://releases.interpretica.io/isabelle-equestrian-release/main-latest/equestrian-main-latest.tar.xz"
+url_release_intranet="https://releases.interpretica.io/isabelle-intranet-release/main-latest/intranet-main-latest.tar.xz"
+
 case "$flavour" in
     equestrian)
-        target_data_gen="$url_datagen_equestrian"
+        target_release="$url_release_equestrian"
         ;;
     intranet)
-        target_data_gen="$url_datagen_intranet"
+        target_release="$url_release_intranet"
         ;;
     *)
         echo "Unknown flavour: $flavour" >&2
         exit 1
 esac
 
-${TOP_DIR}/service.sh stop
+pushd ${DISTR_DIR} > /dev/null
+
+touch wget_tmp
+chmod 600 wget_tmp
+echo "user=$user" > wget_tmp
+echo "password=$password" >> wget_tmp
+WGETRC=./wget_tmp wget "${target_release}" -o release.tar.xz || fail "Failed to download release"
+${TOP_DIR}/service.sh stop || fail "Failed to stop service"
+tar xvf release.tar.xz
+rm release.tar.xz
+rm wget_tmp
+
+popd > /dev/null
+
+${TOP_DIR}/service.sh start || fail "Failed to start service"
