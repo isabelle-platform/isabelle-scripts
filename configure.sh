6f867c48c3c6a74a1f5444fd1904fd5385b25b25
Maxim Menshikov <maxim@menshikov.org>
2024-02-25 21:37:29 +0000

    Add configure.sh

---
+++ configure.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+TOP_DIR="$(cd "$(dirname "$(which "$0")")" ; pwd -P)"
+cd "${TOP_DIR}"
+
+. ./lib_header.sh
+
+while test -n "$1" ; do
+    case $1 in
+        --flavour)
+            echo "$2" > "${DISTR_DIR}/.flavour"
+            shift 1
+            ;;
+        --db-port)
+            echo "$2" > "${DISTR_DIR}/.db_port"
+            shift 1
+            ;;
+        --core-port)
+            echo "$2" > "${DISTR_DIR}/.core_port"
+            shift 1
+            ;;
+        --pub-fqdn)
+            echo "$2" > "${DISTR_DIR}/.pub_fqdn"
+            shift 1
+            ;;
+        --pub-url)
+            echo "$2" > "${DISTR_DIR}/.pub_url"
+            shift 1
+            ;;
+        --cert-owner)
+            echo "$2" > "${DISTR_DIR}/.cert_owner"
+            shift 1
+            ;;
+        --srv-port)
+            echo "$2" > "${DISTR_DIR}/.srv_port"
+            shift 1
+            ;;
+        --machine-type)
+            echo "$2" > "${DISTR_DIR}/.machine_type"
+            shift 1
+            ;;
+    esac
+    shift 1
+done
